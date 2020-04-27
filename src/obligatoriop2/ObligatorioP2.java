c00b66c71b73da56491fbebc6bd05265c00676f4
seba3227 <64437760+seba3227@users.noreply.github.com>
2020-04-27 17:08:10 -0300

    puse sout

--- src/obligatoriop2/ObligatorioP2.java
+++ src/obligatoriop2/ObligatorioP2.java
@@ -16,6 +16,7 @@ public class ObligatorioP2 {
      */
     public static void main(String[] args) {
         System.out.println("hola, probando cambio");
+        System.out.println("seba ");
     }
     
 }
