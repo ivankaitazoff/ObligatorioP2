3bdcbd5b6bc06c1584d755758a03bc9c0bd7f39f
Ivan Kaitazoff <ivankaitazoff@gmail.com>
2020-04-27 16:44:40 -0300

    prueba cambio 1

--- src/obligatoriop2/ObligatorioP2.java
+++ src/obligatoriop2/ObligatorioP2.java
@@ -15,7 +15,7 @@ public class ObligatorioP2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola, probando cambio");
     }
     
 }
