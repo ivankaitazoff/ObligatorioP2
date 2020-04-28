f556cf675fe7f7076b94a4285964206969912ed5
Ivan Kaitazoff <ivankaitazoff@gmail.com>
2020-04-28 17:56:44 -0300

    Validacion numero andando(Sacada del obligatorio anterior)

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -27,11 +27,11 @@ public class Interfaz {
     
     public static void crearJugador(){
         System.out.println("Ingresar nombre");
-        String nombre=validarTexto();
+        String nombre=validarString();
         System.out.println("Ingresar edad");
-        int edad=validarNumero(10, 100);
+        int edad=validacionNumero(10, 100);
         System.out.println("Ingresar alias");
-        String alias=validarTexto();
+        String alias=validarString();
         
         //imprimir datos
         System.out.println(nombre);
@@ -40,13 +40,13 @@ public class Interfaz {
         
     }
     
-    public static String validarTexto(){
+    public static String validarString(){
     Scanner scanner= new Scanner(System.in);
     String dato=scanner.nextLine();
     return dato;
     }
     
-    public static int validarNumero(int minimo, int maximo){
+    /* public static int validarNumero(int minimo, int maximo){
         int numero=0;
         Boolean datoValido = false;
         Scanner scanner= new Scanner(System.in);
@@ -67,5 +67,28 @@ public class Interfaz {
             }
         }
     return numero;
+    }   */
+    
+    public static int validacionNumero(int min, int max) {
+        boolean ok = false;
+        while (!ok) {
+            String palabra = validarString();
+            try {
+                int num = Integer.parseInt(palabra);
+                if (num >= min && num <= max) {
+                    return num;
+                } else {
+                    Toolkit.getDefaultToolkit().beep();  
+                    System.out.print("Error, ingrese un valor entre: " + min + " y " + max + ": ");
+                }
+            } catch (Exception e) {
+                Toolkit.getDefaultToolkit().beep();  
+                System.out.print("Error, tipo de dato incorrecto, volver a ingresar: ");
+            }
+        }
+        return 0;
     }
+    
+    
+    
 }
