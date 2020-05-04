e14d0e88648c0b0a5bc6fbb7f7529f3eafeb1784
seba3227 <64437760+seba3227@users.noreply.github.com>
2020-05-04 18:17:23 -0300

    creo array de dados en tablero

--- src/dominio/Tablero.java
+++ src/dominio/Tablero.java
@@ -1,7 +1,7 @@
 package dominio;
 
 public class Tablero {
-
+    int [] dados = new int[5];
     int[][] tablero = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15},
     {16, 17, 18, 19, 20}};
 
